fc2ac45fa5b0d933f292d41c938dcbeaf8685658
unknown <irginazwamustofa@gmail.com>
2025-07-23 14:40:17 +0700

    update baru

--- lib/ourSkills.tsx
+++ lib/ourSkills.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function OurSkills() {
   return (
-    <div className='flex flex-col justify-center items-center relative '>
+    <div className='flex flex-col justify-center items-center relative w-full'>
         <h1 className='text-[#005CB2] text-4xl font-extrabold'>Our Skills</h1>
-        <div className=''>
+        <div className='w-full h-150 bg-[#D9DFFC] mt-8'>
 
         </div>
     </div>
