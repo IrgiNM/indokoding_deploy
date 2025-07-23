614280c25450804caa9d4027f3a679e93ed32512
unknown <irginazwamustofa@gmail.com>
2025-07-23 15:20:40 +0700

    update home, dan ourwork

--- lib/navbar.tsx
+++ lib/navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
   ];
 
   return (
-    <div className={`h-20 flex flex-row justify-center items-start gap-4 pt-7 fixed top-0 w-full z-50 transition-all duration-300
+    <div className={`h-20 flex flex-row justify-center items-start gap-4 pt-5 fixed top-0 w-full z-50 transition-all duration-300
       ${scrolled ? 'bg-white shadow-md' : 'bg-gradient-to-b from-white to-transparent'}`}>
       {nav.map((x, y) => (
         <NavLink key={y} warna={bgNav === x.isi ? x.active : x.warna} bg={x.bg} klik={x.link}>
