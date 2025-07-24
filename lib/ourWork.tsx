a53003e504a27b422844cd151a48706db4f0870b
unknown <irginazwamustofa@gmail.com>
2025-07-24 10:22:33 +0700

    update fix ourskills

--- lib/ourWork.tsx
+++ lib/ourWork.tsx
@@ -58,7 +58,7 @@ export default function OurWork() {
     },
   ]
   return (
-    <div className='w-full pb-100 relative flex flex-col items-start justify-center'>
+    <div className='w-full pb-50 relative flex flex-col items-start justify-center'>
       <div className='w-full flex flex-col items-end justify-end'>
         <h1 className='text-4xl text-[#128900] font-extrabold mr-50'>Our Work</h1>
         <p className='mr-50'>A collection of website we've made with passion</p>
---
+++ type/typeSkill.ts
@@ -0,0 +1,11 @@
+export type TypeSkill = {
+    id: number;
+    image: string;
+    pin: string;
+    title: string;
+    description: string;
+    warnaTitle: string;
+    warnaBg: string;
+    rotate: string;
+    position: string;
+}
