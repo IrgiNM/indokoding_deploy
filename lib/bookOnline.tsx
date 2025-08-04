f246de7dc49a62c0c446c0876d61801ea95be733
annisafzahra <annisafathimatuzzahra@gmail.com>
2025-08-04 13:39:05 +0700

    responsive book online

--- lib/bookOnline.tsx
+++ lib/bookOnline.tsx
@@ -25,12 +25,12 @@ export function BookOnline() {
 
     return (
         <>
-        <div className="flex flex-row relative items-center gap-12 mt-35">
-            <Image width={140} height={140} src="/assets/image/consule.png" alt="consule" className="w-90 h-90" />
-            <div className="flex flex-col items-start justify-center">
-                <h1 className="text-6xl font-extrabold text-[#00466C] mb-4">BOOK ONLINE</h1>
-                <p className="font-semibold text-[#00466C] text-[18px]">Free Online Consultation</p>
-                <p className="text-[#00466C] text-[18px] tracking-wide custom-word-spacing mb-4">1hr Consultation | Skype or email at <br></br> info@indokoding.com</p>
+        <div className="lg:flex lg:flex-row lg:relative lg:items-center lg:gap-12 lg:mt-35 flex flex-col relative items-center gap-12 mt-35">
+            <Image width={140} height={140} src="/assets/image/consule.png" alt="consule" className=" lg:w-90 lg:h-90 w-55 h-55 -mb-10" />
+            <div className="lg:flex lg:flex-col lg:items-start lg:justify-center flex flex-col items-center justify-center">
+                <h1 className="lg:text-6xl lg:text-start lg:font-extrabold lg:text-[#00466C] lg:mb-4 text-4xl text-center font-extrabold text-[#00466C] mb-4">BOOK ONLINE</h1>
+                <p className="lg:font-semibold lg:text-[#00466C] lg:text-[18px] lg:text-start font-semibold text-[#00466C] text-[12px] tracking-wide text-center">Free Online Consultation</p>
+                <p className="lg:text-[#00466C] lg:text-[18px] lg:tracking-wide lg:text-start lg:custom-word-spacing lg:mb-4 text-[#00466C] text-[12px] text-center mb-4">1hr Consultation | Skype or email at <br></br> info@indokoding.com</p>
                 {/* <button className="bg-[#FFBC48] w-35 text-white font-semibold px-4 py-2 rounded-md hover:bg-gradient-to-b hover:from-[#FFBC48] hover:to-[#ff8903] hover:font-bold transition duration-200 mt-3">
                     BOOK NOW
                 </button> */}
@@ -51,9 +51,9 @@ export function BookOnline() {
                 </Button>
                 {/* <Button variant="contained">Hello world</Button> */}
             </div>
-            <div className="absolute rounded-full left-195 -top-37 -z-1 bg-[#F1D6FF] w-100 h-100"></div>
-            <div className="absolute rounded-full left-150 top-70 -z-1 bg-[#D6FFDC] w-40 h-40"></div>
-            <div className="absolute rounded-full -left-60 -top-65 -z-1 bg-[#D6E9FF] w-80 h-80"></div>
+            <div className="lg:absolute lg:rounded-full lg:left-195 lg:-top-37 lg:-z-1 lg:bg-[#F1D6FF] lg:w-100 lg:h-100 absolute rounded-full left-40 -top-32 -z-1 bg-[#F1D6FF] w-75 h-75"></div>
+            <div className="lg:absolute lg:rounded-full lg:left-150 lg:top-70 lg:-z-1 lg:bg-[#D6FFDC] lg:w-40 lg:h-40 absolute rounded-full -left-5 top-70 -z-1 bg-[#D6FFDC] w-25 h-25"></div>
+            <div className="lg:absolute lg:rounded-full lg:-left-60 lg:-top-65 lg:-z-1 lg:bg-[#D6E9FF] lg:w-80 lg:h-80 absolute rounded-full -left-60 -top-65 -z-1 bg-[#D6E9FF] w-80 h-80"></div>
         </div>
 
         <Dialog
@@ -65,11 +65,10 @@ export function BookOnline() {
             maxWidth="md"
             PaperProps={{
                 sx: {
-                    borderRadius: 3, // bisa juga '16px' atau angka (1 = 8px, 2 = 16px, dst)
+                    borderRadius: 3, 
                     width: '2000px',
                     height: '600px'
                   },
-                // className: 'w-[2000px] h-130' // atau pakai sx jika mau
             }}
             onClose={handleClose}
             aria-describedby="alert-dialog-slide-description"
