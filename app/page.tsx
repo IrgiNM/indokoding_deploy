b860079e8adaf5956d2d97ffcd00f71e6b23189a
unknown <irginazwamustofa@gmail.com>
2025-08-04 10:03:36 +0700

    update navbar ourwork

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,32 @@ import Footer from '@/lib/footer';
 import AboutUs from '@/lib/aboutUs';
 import ContactUs from '@/lib/contactUs';
 import OurSkills from '@/lib/ourSkills';
-import OurWork from '@/lib/ourWork';
-import ContactUsSm from '@/lib/mobile/contactUsSm';
-import FooterSm from '@/lib/mobile/footerSm';
-import CenterMode from '@/lib/ourWorkCenter';
 import OurWorkCenter from '@/lib/ourWorkCenter';
+import { useRef } from "react";
+
 
 export default function page() {
+  const ourWorkRef = useRef<HTMLDivElement>(null);
+
+  const scrollToRef = (ref: React.RefObject<HTMLElement | null>) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='w-full flex flex-col items-center justify-center overflow-hidden'>
-      <Navbar page="Home"/>
-      <Home/>
-      {/* <OurWork/> */}
-      <OurWorkCenter/>
-      {/* <ExampleCarousel /> */}
-      <AboutUs/>
-      <OurSkills/>
-      <Service/>
-      <ContactUs/>
-      {/* <ContactUsSm /> */}
+      <Navbar
+        page="home"
+        onNavClick={{
+          ourWork: () => scrollToRef(ourWorkRef),
+        }}
+      />
+      <Home />
+      <OurWorkCenter ref={ourWorkRef}/>
+      <AboutUs  />
+      <OurSkills  />
+      <Service  />
+      <ContactUs  />
       <Footer />
-      {/* <FooterSm /> */}
     </div>
   )
 }
