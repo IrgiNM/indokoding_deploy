01f59c48f5f27f224607af7ae6831fbd7c06d4ec
tantrihandayani <tantriyandayani@gmail.com>
2025-08-12 09:33:46 +0700

    tantr

--- lib/ourWorkCenter.tsx
+++ lib/ourWorkCenter.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-coverflow";
+// import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 // import "./styles.css";
 // import required modules
