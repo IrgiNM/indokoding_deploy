c26a81d6124bf99ecce3d7fb86d320ed1bd6886a
tantrihandayani <tantriyandayani@gmail.com>
2025-09-04 16:14:22 +0700

    update alamat di contact us

--- lib/contactUs.tsx
+++ lib/contactUs.tsx
@@ -185,7 +185,7 @@ function ContactUsComponent(props: object, ref: React.Ref<HTMLDivElement>) {
             <div className=" lg:flex lg:flex-col lg:md:flex-row lg:gap-4 w-full flex flex-col md:flex-row gap-4">
               <input
                 // onClick={() => setShowPopup(true)}
-                className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#4F006C] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#4F006C] w-full"
+                className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#181F38] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#181F38] w-full"
                 type="email"
                 placeholder="Name"
                 id="username"
@@ -195,7 +195,7 @@ function ContactUsComponent(props: object, ref: React.Ref<HTMLDivElement>) {
               />
               <input
                 // onClick={() => setShowPopup(true)}
-                className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#4F006C] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#4F006C] w-full"
+                className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#181F38] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#181F38] w-full"
                 type="text"
                 placeholder="Email"
                 id="email"
@@ -207,7 +207,7 @@ function ContactUsComponent(props: object, ref: React.Ref<HTMLDivElement>) {
 
             <input
               // onClick={() => setShowPopup(true)}
-              className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#4F006C] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#4F006C] w-full"
+              className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-3 lg:text-[#181F38] lg:w-full bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-2 text-[#181F38] w-full"
               type="text"
               placeholder="Subject"
               id="subject"
@@ -218,7 +218,7 @@ function ContactUsComponent(props: object, ref: React.Ref<HTMLDivElement>) {
 
             <textarea
               // onClick={() => setShowPopup(true)}
-              className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-4 lg:text-[#4F006C] lg:h-40 lg:w-full lg:resize-none bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-4 text-[#4F006C] h-40 w-full resize-none"
+              className=" lg:bg-[#D9DFFC] lg:rounded-3xl lg:text-sm lg:px-5 lg:py-4 lg:text-[#181F38] lg:h-40 lg:w-full lg:resize-none bg-[#D9DFFC] rounded-lg text-[12px] px-5 py-4 text-[#181F38] h-40 w-full resize-none"
               placeholder="Message"
               id="message"
               name="message"
@@ -234,44 +234,54 @@ function ContactUsComponent(props: object, ref: React.Ref<HTMLDivElement>) {
           </div>
 
           
-          <div className=" lg:w-full lg:md:w-1/3 lg:flex lg:flex-col lg:gap-4 lg:text-[#4F006C] w-full md:w-1/3 flex flex-col justify-center items-center gap-4 text-[#4F006C]">
+          <div className=" lg:w-full mt-3 lg:md:w-1/3 lg:flex lg:flex-col lg:gap-4 lg:text-[#4F006C] w-full md:w-1/3 flex flex-col justify-center items-center gap-4 text-[#4F006C]">
             
-            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-4 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-4 pl-7 rounded-xl w-full">
-              <Image width={140} height={140} src="/icon-jam.svg" alt="" className=' lg:w-6 lg:mr-5 w-6 mr-5'/>
+            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-2 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-4 pl-7 rounded-xl w-full">
+              <Image width={140} height={140} src="/icon-jam.svg" alt="" className=' lg:w-5 lg:mr-4 w-6 mr-5'/>
               <div>
                 <p className="font-semibold text-[12px] text-[#181F38]">Monday - Friday</p>
                 <p className="lg:text-sm text-[12px] text-[#181F38]">7 AM - 6 PM</p>
               </div>
             </div>
             
-            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-4 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-4 pl-7 rounded-xl w-full">
-              <Image width={140} height={140} src="/icon-phone.svg" alt="" className=' lg:w-6 lg:mr-5 w-6 mr-5'/>
+            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-3 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-4 pl-7 rounded-xl w-full">
+              <Image width={140} height={140} src="/icon-phone.svg" alt="" className=' lg:w-4 lg:mr-4 w-6 mr-5'/>
               <div>
                 <p className="lg:font-semibold font-semibold text-[12px] text-[#181F38]">+62274 540448</p>
-                <p className="lg:text-sm text-[12px] text-[#181F38]">+622745306395</p>
+                
               </div>
             </div>
 
             
-            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-4 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-4 pl-7 rounded-xl w-full">
-              <Image width={140} height={140} src="/icon-email.svg" alt="" className=' lg:w-6 lg:mr-5 w-6 mr-5'/>
+            <div className=" lg:bg-[#D9DFFC] lg:flex lg:flex-row lg:items-center lg:p-3 lg:pl-7 lg:rounded-2xl bg-[#D9DFFC] flex flex-row items-center p-3 pl-7 rounded-xl w-full">
+              <Image width={140} height={140} src="/icon-email.svg" alt="" className=' lg:w-4 lg:mr-4 w-4 mr-4'/>
               <div>
                 <p className="lg:text-sm text-[12px] text-[#181F38]">info@indokoding.com</p>
               </div>
             </div>
 
+            <div className='text-[10px] text-[#181F38]'>
+              <p>📍Office: Jl. Ngadinegaran MJ III No.144, Mantrijeron,
+              Mantrijeron, Yogyakarta 55143</p>
+            </div>
+            <div className='text-[10px] text-[#181F38]'>
+              <p>📍Factory: Prenggan, Sidokarto,
+                 Godean, Sleman Regency,Yogyakarta 55264</p>
+            </div>
+
             
-            <div className="lg:mt-4 mt-4 flex flex-col justify-center items-center">
-              <p className=" lg:mb-2 lg:text-extrabold mb-2 text-extrabold text-[#181F38] text-[12px]">Follow our social media</p>
+            <div className="lg:mt-1 mt-1 flex flex-col justify-center items-center">
+              <p className=" lg:mb-2 lg:text-extrabold mb-2 text-extrabold text-[#181F38] text-[12px] font-bold">Follow our social media</p>
               <div className=" lg:flex lg:gap-4 flex gap-4">
                 <div className=" lg:bg-[#D9DFFC] cursor-pointer lg:w-10 lg:h-10 lg:rounded-full lg:flex lg:items-center lg:justify-center bg-[#D9DFFC] w-10 h-10 rounded-full flex items-center justify-center">
+                  <a href=" https://www.instagram.com/indokoding/" target="_blank" rel="noopener noreferrer">
                   <Image width={140} height={140} src="/fb-black.svg" alt="" className=' lg:w-3 w-3'/>
+                  </a>
                 </div>
                 <div className=" lg:bg-[#D9DFFC] cursor-pointer lg:w-10 lg:h-10 lg:rounded-full lg:flex lg:items-center lg:justify-center bg-[#D9DFFC] w-10 h-10 rounded-full flex items-center justify-center">
+                  <a href="http://www.instagram.com/indokoding/" target="_blank" rel="noopener noreferrer">
                   <Image width={140} height={140} src="/ig-black.svg" alt="" className='lg:w-5 w-5'/>
-                </div>
-                <div className=" lg:bg-[#D9DFFC] cursor-pointer lg:w-10 lg:h-10 lg:rounded-full lg:flex lg:items-center lg:justify-center bg-[#D9DFFC] w-10 h-10 rounded-full flex items-center justify-center">
-                  <Image width={140} height={140} src="/x-black.svg" alt="" className='lg:w-4 w-4'/>
+                  </a>
                 </div>
               </div>
             </div>
